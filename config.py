a192f90e4174a1f715027c715a2c04bf5f1aeddb
procrastinando <74340724+procrastinando@users.noreply.github.com>
2025-12-10 21:36:14 +0800

    new updates

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ TELEGRAM_MSG_LIMIT = 4096
 DEFAULT_MODEL = { "name": "meta-llama/llama-4-maverick-17b-128e-instruct", "temperature": 1.0 }
 DEFAULT_MAX_TOKENS = 8192
 
-MODELS = { "Kimi k2": "moonshotai/kimi-k2-instruct", "📷 Llama 4 Maverick": "meta-llama/llama-4-maverick-17b-128e-instruct", "GPT-Oss 120B": "openai/gpt-oss-120b" }
+MODELS = { "Kimi k2": "moonshotai/kimi-k2-instruct-0905", "📷 Llama 4 Maverick": "meta-llama/llama-4-maverick-17b-128e-instruct", "GPT-Oss 120B": "openai/gpt-oss-120b" }
 MODEL_MAX_TOKENS = { "moonshotai/kimi-k2-instruct": 16384, "meta-llama/llama-4-maverick-17b-128e-instruct": 8192, "openai/gpt-oss-120b": 65536 }
 
 VISION_MODELS = ["meta-llama/llama-4-maverick-17b-128e-instruct"]
